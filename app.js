00322a907a3d4efa849256a612e46573fb887f70
ryantranj@gmail.com <ryantranj@gmail.com>
2020-09-02 22:15:05 -0700

    changed hero classes to sections to test opacity load in animation on scroll in js. Also added slide in animation temporarily

--- app.js
+++ app.js
@@ -1,4 +1,34 @@
-let a = 'hello world';
-let b = 'I\'ve changed';
-console.log(a);
-console.log(b);
+let section = document.querySelectorAll('.section');
+
+function isVisible(section) {
+  let elementBox = section.getBoundingClientRect();
+  let distanceFromTop = -100;
+  if (elementBox.top - window.innerHeight < distanceFromTop || (window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function scanDocument() {
+  let sectionList = document.querySelectorAll('.hidden');
+  sectionList.forEach(function(section) {
+    if (isVisible(section)) {
+      section.classList.remove('hidden');
+      section.querySelector('.inner').classList.add('slide');
+    }
+  });
+}
+
+
+window.addEventListener("scroll", throttle(scanDocument, 50));
+
+function throttle(fn, wait) {
+  var time = Date.now();
+  return function() {
+    if ((time + wait - Date.now()) < 0) {
+      fn();
+      time = Date.now();
+    }
+  }
+}
