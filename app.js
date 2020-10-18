c53c2cfd800218cf4330a1e82988e8d58e1a41bf
ryantranj@gmail.com <ryantranj@gmail.com>
2020-10-18 11:30:16 -0700

    Added functionalty and animation to experience tabs. optimized header code.

--- app.js
+++ app.js
@@ -32,3 +32,48 @@ function throttle(fn, wait) {
     }
   }
 }
+
+$('#cres').on('click', () => {
+  if(!$('#tab1').hasClass('animateAppear')) {
+    $('#aqi, #cpp').parent().removeClass('tab-active');
+    $('#cres').parent().addClass('tab-active');
+    $('#tab2, #tab3').removeClass('animateAppear').addClass('animateDisappear tab-hidden');
+    $('#tab1').removeClass('animateDisappear').addClass('animateAppear');
+  }
+});
+$('#aqi').on('click', () => {
+  if(!$('#tab2').hasClass('animateAppear')) {
+    $('#cres, #cpp').parent().removeClass('tab-active');
+    $('#aqi').parent().addClass('tab-active');
+    $('#tab1, #tab3').removeClass('animateAppear').addClass('animateDisappear tab-hidden');
+    $('#tab2').removeClass('animateDisappear').addClass('animateAppear');
+  }
+});
+$('#cpp').on('click', () => {
+  if(!$('#tab3').hasClass('animateAppear')) {
+    $('#cres, #aqi').parent().removeClass('tab-active');
+    $('#cpp').parent().addClass('tab-active');
+    $('#tab1, #tab2').removeClass('animateAppear').addClass('animateDisappear tab-hidden');
+    $('#tab3').removeClass('animateDisappear').addClass('animateAppear');
+  }
+});
+
+  // $('#home').on('click', function() {
+  //   $('.container').hide();
+  //   $('.container.home').css('display', 'flex');
+  //   $('.nav').find('.active-link').removeClass('active-link');
+  //   $('#home').addClass('active-link');
+  // });
+  // $('#faq').on('click', function() {
+  //   $('.container').hide();
+  //   $('.container.faq').css('display', 'block');
+  //   $('.content').css('display', 'flex');
+  //   $('.nav').find('.active-link').removeClass('active-link');
+  //   $('#faq').addClass('active-link');
+  // });
+  // $('#myAccount').on('click', function() {
+  //   $('.container').hide();
+  //   $('.container.myAccount').css('display', 'flex');
+  //   $('.nav').find('.active-link').removeClass('active-link');
+  //   $('#myAccount').addClass('active-link');
+  // });
