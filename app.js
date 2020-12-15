3a7a344cc1062e9a5d85d1acfc775d69033a98d1
ryantranj@gmail.com <ryantranj@gmail.com>
2020-12-14 16:52:31 -0800

    added responsive css for homepage from desktop to tablet. added scrolling animation to anchor point.

--- app.js
+++ app.js
@@ -70,18 +70,17 @@ $('#cpp').on('click', () => {
   }
 });
 
-$('#work').on('click', function () {
-  if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
-
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      if (target.length) {
-          $('html,body').animate({
-              scrollTop: target.offset().top
-          }, 1000);
-          return false;
-      }
+$(".scroll").on("click", function(event){
+  event.preventDefault();
+  //calculate destination place
+  var dest=0;
+  if($(this.hash).offset().top > $(document).height()-$(window).height()){
+       dest=$(document).height()-$(window).height();
+  }else{
+       dest=$(this.hash).offset().top - 25;
   }
+  //go to destination
+  $('html,body').animate({scrollTop:dest}, 200,'swing');
 });
 
 // git add .
